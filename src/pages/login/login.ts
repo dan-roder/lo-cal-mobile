01e01a47953c79afbe1d9793eac42f6156895d37
Ken Crocken <kcrocken@repequity.com>
2017-08-11 09:00:16 -0400

    adds login form

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,20 +1,70 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { IonicPage, NavController, NavParams, ToastController, LoadingController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+
+    selector: 'page-login',
+    templateUrl: 'login.html',
+
 })
 export class LoginPage {
 
-  constructor( 
-    public navCtrl: NavController, 
-    public navParams: NavParams
-  ) {}
+    loading: boolean;
+    loginForm: FormGroup;
+    email: AbstractControl;
+    password: AbstractControl;
+    error: any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+    constructor( 
+
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        private loadingController: LoadingController,
+        private toastController: ToastController,
+        private fb: FormBuilder
+
+    ) {
+    
+        this.loginForm = this.fb.group({
+            'email': ['', Validators.compose([Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)])],
+            'password': ['', Validators.compose([Validators.required, Validators.minLength(1)])]
+        });
+
+        this.email = this.loginForm.controls['email'];
+        this.password = this.loginForm.controls['password'];
+
+    }
+
+    ionViewDidLoad() {
+
+        console.log('ionViewDidLoad LoginPage');
+
+    }
+
+    login() {
+        if( this.loginForm.valid ) {
+            console.log( this.loginForm.valid, this.email.value, this.password.value );
+            let message = `Form Valid, email: ${this.email.value}, password: ${this.password.value}`;
+            let toast = this.toastController.create({
+                message: message,
+                duration: 6000,
+                position: 'bottom'
+            });
+            toast.present();
+
+        } else {
+            console.log( this.loginForm.valid );
+            let message = `Form not valid`;
+            let toast = this.toastController.create({
+                message: message,
+                duration: 6000,
+                position: 'bottom'
+            });
+            toast.present();
+        }
+        
+    }
 
 }
