2816ed0678b980496036da74a00a3661d4db6710
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-02 16:21:01 -0400

    add safe html pipe

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -4,19 +4,22 @@ import { TruncatePipe } from './truncate.pipe';
 import { TrimHtmlPipe } from './trim-html.pipe';
 import { AllergensPipe } from './allergens.pipe';
 import { CardImagePipe } from './image.pipe';
+import { SafeHtml } from './safe-html.pipe'
 @NgModule({
     declarations: [
         TruncatePipe,
         TrimHtmlPipe,
         AllergensPipe,
-        CardImagePipe
+        CardImagePipe,
+        SafeHtml
     ],
     imports: [CommonModule],
     exports: [
         TruncatePipe,
         TrimHtmlPipe,
         AllergensPipe,
-        CardImagePipe
+        CardImagePipe,
+        SafeHtml
     ]
 })
 export class PipesModule { }
