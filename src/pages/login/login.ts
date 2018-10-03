a3fc59fb0e39c40f9475b398570a3a2b19f1e85d
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-03 14:05:29 -0400

    login fix

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -31,6 +31,7 @@ export class LoginPage {
         private fb:                FormBuilder
 
     ) {
+
         this.loginForm = this.fb.group({
             'email': ['', Validators.compose([Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)])],
             'password': ['', Validators.compose([Validators.required, Validators.minLength(1)])]
@@ -38,7 +39,7 @@ export class LoginPage {
 
         this.email = this.loginForm.controls['email'];
         this.password = this.loginForm.controls['password'];
-        console.log( this.email.invalid && this.email.dirty );
+
     }
 
     ionViewDidLoad() {
@@ -64,10 +65,8 @@ export class LoginPage {
 
                 // this.navCtrl.push(TabsComponent)
                 // this.navCtrl.popToRoot();
-                this.navCtrl.setRoot('TabsComponent');
+                this.navCtrl.setRoot(TabsComponent);
                 console.log( response );
-                this.navCtrl.pop();
-
 
             }, error => {
 
