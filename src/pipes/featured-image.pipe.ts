60fa8d3e287f3fb140cb52eefeae5fd8ee575c92
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-04 19:06:02 -0400

    add featured image pipe

---
+++ src/pipes/featured-image.pipe.ts
@@ -0,0 +1,19 @@
+import { WordPressProvider } from './../providers/word-press/word-press';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'featuredImage'
+})
+export class FeaturedImagePipe implements PipeTransform {
+
+  constructor(private wpService: WordPressProvider){}
+
+  transform(mediaData: any, args?: any): any {
+    if(args == 'alt'){
+      return mediaData['wp:featuredmedia'][0].alt_text;
+    }
+
+    return mediaData['wp:featuredmedia'][0].media_details.sizes[args].source_url;
+  }
+
+}
