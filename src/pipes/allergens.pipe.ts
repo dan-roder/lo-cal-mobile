b0f93dd6a1f4d7cad6604fd664fa5726a68b8c65
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-03 23:37:07 -0400

    fix allergens pipe

--- src/pipes/allergens.pipe.ts
+++ src/pipes/allergens.pipe.ts
@@ -1,32 +1,42 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
 
 @Pipe({
-    name: 'allergens'
+  name: 'allergens'
 })
 export class AllergensPipe implements PipeTransform {
 
-    transform(value: any, args?: any): any {
-        // Create object to be returned and iterated
-        let obj = {};
-        // Find instance of Allergen brackets and split the string into array
-        //  Item 1 is the full description
-        //  Item 2 is the allergens
-        if (value.indexOf('[') !== -1) {
-            let strArray = value.split('[');
-            obj['description'] = strArray[0];
-            // If the item has a ] in it slice it off so we're left with just allergen codes
-            if (strArray[1] !== undefined) {
-                strArray[1] = strArray[1].slice(0, -1);
-                // Split allergens into subArray based on underscores
-                strArray[1] = strArray[1].split('_');
-                obj['allergens'] = strArray[1];
-            }
-        // If no allergen brackets are found, just build the obj from the value
-        } else {
-            obj['description'] = value;
-            obj['allergens'] = "";
-        }
-        return obj;
+  transform(value: any, args?: any): any {
+    // Create object to be returned and iterated
+    let obj = {};
+
+    // Find instance of Allergen brackets and split the string into array
+    //  Item 1 is the full description
+    //  Item 2 is the allergens
+    if(value.indexOf('[') !== -1){
+      let matches = value.match(/(.*)(\[.*\])(.*)/);
+      obj['description'] = matches[1].trim();
+
+      // If the item has a ] in it slice it off so we're left with just allergen codes
+      if(matches[2] !== undefined){
+        matches[2] = matches[2].slice(0, -1);
+        matches[2] = matches[2].substr(1);
+        // Split allergens into subArray based on underscores
+        matches[2] = matches[2].split('_');
+
+        obj['allergens'] = matches[2];
+      }
+
+      // If a 3rd matches group exists there was text beyond the allergens
+      if(matches[3] !== undefined){
+        obj['additionalInfo'] = matches[3].trim();
+      }
     }
+    else{
+      obj['description'] = value.trim();
+    }
+
+    return obj;
+  }
 
 }
