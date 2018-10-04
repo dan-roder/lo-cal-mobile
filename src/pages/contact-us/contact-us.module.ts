ba05c7b404821ac511094ead8e9cfc92a39bbfc7
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-03 21:40:34 -0400

    pull in pipes module

--- src/pages/contact-us/contact-us.module.ts
+++ src/pages/contact-us/contact-us.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ContactUsPage } from './contact-us';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ContactUsPage } from './contact-us';
   ],
   imports: [
     IonicPageModule.forChild(ContactUsPage),
+    PipesModule
   ],
   exports: [
     ContactUsPage
