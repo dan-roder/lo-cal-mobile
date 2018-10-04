59b1e385e866a764ad20f3b358c0bec0dd4d6440
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-04 19:06:23 -0400

    adding blog index page

--- src/pages/blog/blog.ts
+++ src/pages/blog/blog.ts
@@ -1,3 +1,4 @@
+import { WordPressProvider } from './../../providers/word-press/word-press';
 import { Component } from '@angular/core';
 import { NavController, NavParams, IonicPage, LoadingController } from 'ionic-angular';
 import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
@@ -12,11 +13,23 @@ import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 })
 export class BlogPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  public featuredImage : any;
+  public allBlogPosts : any;
+
+  constructor(
+      public navCtrl: NavController,
+      public navParams: NavParams,
+      private wordpressService: WordPressProvider
+
+  ) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BlogPage');
+    this.wordpressService.getCustomPostType('blog_post').subscribe(posts => {
+        this.allBlogPosts = JSON.parse(posts._body);
+        console.log(this.allBlogPosts);
+
+      })
   }
   // must be present with auto-unsubscribe even if empty
   ngOnDestroy() {
