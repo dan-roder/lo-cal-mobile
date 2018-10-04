67cca38c9fd139b3cb95a89b5273cadd4ffcff67
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-04 10:27:40 -0400

    set up, adding dependencies and declaring variables

--- src/pages/password-reset/password-reset.ts
+++ src/pages/password-reset/password-reset.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, IonicPage, LoadingController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
+import { CustomerProvider } from '../../providers/customer/customer';
+import { InPasswordReset } from '../../models/customer';
 
-// @AutoUnsubscribe()
+@AutoUnsubscribe()
 
 @IonicPage()
 
@@ -13,16 +15,33 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PasswordResetPage {
 
+    public passwordResetForm: FormGroup;
+    public forcePasswordResetForm: FormGroup;
+    public foundSecurityQuestion: boolean = false;
+    public errorData: any = {};
+    public securityQuestion: string = '';
+    public switchForms: boolean = false;
+    public submittedOnce: boolean = false;
+    public passwordResetError: string = '';
+    public passwordSuccess: boolean = false;
+    public forcePasswordSuccess: boolean = false;
+    public forceProcessing: boolean = false;
+    public processing: boolean = false;
+    public securityQuestionError: string = '';
+    public forcePasswordError: string = '';
 
     constructor(
 
         public navCtrl: NavController,
-        public navParams: NavParams
+        public navParams: NavParams,
+        private fb: FormBuilder,
+        private customerService: CustomerProvider
 
     ) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PasswordResetPage');
+
   }
 
 }
