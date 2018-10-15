55dda1667ebc7fbd78c8ff3922e7d6c8e6013c60
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-15 16:56:46 -0400

    add pipes module

--- src/pages/checkout-review/checkout-review.module.ts
+++ src/pages/checkout-review/checkout-review.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CheckoutReviewPage } from './checkout-review';
+import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { CheckoutReviewPage } from './checkout-review';
   ],
   imports: [
     IonicPageModule.forChild(CheckoutReviewPage),
+    PipesModule
   ],
   exports: [
     CheckoutReviewPage
