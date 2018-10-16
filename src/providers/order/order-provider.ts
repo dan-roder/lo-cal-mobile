86ca65d6a4e589d34b83f4f15b8734d2da687d26
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-15 23:17:54 -0400

    add order provider

---
+++ src/providers/order/order-provider.ts
@@ -0,0 +1,117 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage';
+
+import { Observable } from 'rxjs/Observable';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
+import { Config } from '../../app/app.config';
+
+import { LineItem } from '../../models/LineItem';
+import { RailsInSubmitOrder } from '../../models/Payment';
+import { RailsOrder, Order, OrderResults } from '../../models/Order';
+import { CustomerProvider } from './../customer/customer';
+import { Customer } from '../../models/Customer';
+
+import * as _ from 'lodash';
+
+@AutoUnsubscribe()
+
+@Injectable()
+export class OrderService {
+
+    private userId : string;
+    private _customerInfo : Customer;
+    public _currentOrder : Order;
+    private _promiseDateTime : any;
+
+    constructor(
+        private httpClient: Http,
+        private localStorage: Storage,
+        private config: Config,
+        private customerService: CustomerProvider
+      ) { }
+
+      public putOrder(bagItems: Array<LineItem>): Observable<any>{
+        let orderEndpoint = this.config.railsOrderEndpoint + '/' + this.config.siteId;
+        let order = this.constructOrderObject(bagItems);
+
+        return this.httpClient.put(orderEndpoint, order).map(apiResponse => {
+          return apiResponse;
+        });
+      }
+
+      public async saveOrderToLocalStorage(order){
+        const response = await this.localStorage.set('order', order);
+          return response;
+      }
+
+      protected constructOrderObject(bagItems: Array<LineItem>){
+
+        let order : RailsOrder = {
+          order : {
+            SiteId : this.config.siteId,
+            MenuId : this.config.menuId,
+            PromiseDateTime : this._promiseDateTime,
+            LineItems : bagItems,
+            Customer : this._customerInfo,
+            OrderMode : 'Pickup',
+            PaymentMode : 'Unknown'
+          }
+        }
+
+        return order;
+      }
+
+      public hasOrderBeenCreated(){
+        return this.localStorage.get('order').then(orderDetails => {
+          return orderDetails;
+        });
+      }
+
+      public getPreSavedOrder(): Promise<any>{
+        return this.localStorage.get('order').then(orderDetails => {
+          return orderDetails;
+        })
+      }
+
+      public retrieveTimes(orderMode: string, orderSource: string = '0'): Observable<any>{
+        // Order Sources
+        //  0 - Website
+        //  1 - Mobile (iPhone)
+        //  2 - Mobile Web
+        return this.httpClient.get(this.config.railsTimeEndpoint + `/${orderMode}/${orderSource}/${this.config.menuId}`).map(times => {
+          return times;
+        })
+      }
+
+      public getNextAvailableTime(){
+        return this.httpClient.get(this.config.railsTimeEndpoint + '/1').map(time => {
+          this._promiseDateTime = time;
+          return time.json();
+        })
+      }
+
+      get customerInfo(): Customer{
+        return this._customerInfo;
+      }
+
+      set customerInfo(customer: Customer){
+        this._customerInfo = customer;
+      }
+
+      get currentOrder(): Order{
+        return this._currentOrder;
+      }
+
+      set currentOrder(order: Order){
+        this._currentOrder = order;
+      }
+
+      get promiseDateTime(): any{
+        return this._promiseDateTime;
+      }
+
+      set promiseDateTime(time: any){
+        this._promiseDateTime = time;
+      }
+}
