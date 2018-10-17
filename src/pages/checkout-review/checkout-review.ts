ac28f8ea3dcf3621a1f3006a27388a37650ac90c
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-17 10:31:34 -0400

    adding logic for guest checkout form

--- src/pages/checkout-review/checkout-review.ts
+++ src/pages/checkout-review/checkout-review.ts
@@ -11,10 +11,11 @@ import {
 import {
     FormBuilder,
     Validators,
-    FormGroup
+    FormGroup,
+    AbstractControl
 } from '@angular/forms';
 
-
+import{ Customer } from "../../models/customer"
 import { LineItem } from "../../models/LineItem";
 import { Order } from '../../models/order';
 
@@ -25,6 +26,7 @@ import { BagProvider } from "../../providers/bag/bag";
 import { CustomerProvider } from "../../providers/customer/customer";
 import { WordPressProvider } from './../../providers/word-press/word-press';
 import { OrderService } from './../../providers/order/order-provider';
+import { userInfo } from 'os';
 
 @AutoUnsubscribe()
 
@@ -41,6 +43,7 @@ export class CheckoutReviewPage {
     public isOpen: number = -1;
     public allOrderDetails : Order;
     public timeForm : FormGroup;
+    public guestCheckoutForm: FormGroup;
     public timeSelectBox : string;
     public selectedTime : any;
     public times : any;
@@ -49,13 +52,15 @@ export class CheckoutReviewPage {
     public addressData : any;
     public bagTotalPrice : number;
     public submitAttempted : boolean = false;
+    public hideGuestForm   : boolean = false;
+    public userLoggedIn    : boolean = false;
 
   constructor(
 
       public navCtrl: NavController,
       public navParams: NavParams,
       private bag: BagProvider,
-      private customer: CustomerProvider,
+    //   private customer: CustomerProvider,
       private storage: Storage,
       private alertCtrl: AlertController,
       private loadingController: LoadingController,
@@ -65,123 +70,184 @@ export class CheckoutReviewPage {
 
   ) {
     this.timeForm = fb.group({
-        'pickup-time' : ['', Validators.required]
-      })
+        'pickup-time': ['', Validators.required]
+    })
+
+    this.guestCheckoutForm = fb.group({
+        'guest-first-name': ['', [Validators.required, Validators.maxLength(28)]],
+        'guest-last-name': ['', [Validators.required, Validators.maxLength(28)]],
+        'guest-email': ['', Validators.compose([Validators.required, Validators.pattern(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)])],
+    })
+
+
+    if(!this.navParams.get('guest')) {
+        this.hideGuestForm = true;
+    }
     // let loader = this.loadingController.create({ content: "Loading" });
 
     // loader.present()
 
 
     // loader.dismiss();
-  }
-  ngOnInit() {
-      this.retrievePickupTimes();
-      this.itemsInBag = this.bag.itemsInBag
-      console.log(this.itemsInBag)
+    }
 
-      // Get address data from post
-      if (!this.wpService.addressContent) {
-          this.wpService.getAddressContent().subscribe(addressData => {
-              this.addressData = addressData;
-              this.wpService.addressContent = addressData;
-          })
-      } else {
-          this.addressData = this.wpService.addressContent;
+    ngOnInit() {
+
+        // get pickup times
+        this.retrievePickupTimes();
+
+        // get address info
+        this.getAddress();
+
+        // get customer info
+        this.getCustomer()
+
+        // get bag items from local storage
+        this.itemsInBag = this.bag.itemsInBag
+
+    }
+    public checkoutAsGuest(){
+
+        let loader = this.loadingController.create({ content: "Signing In As Guest" });
+        loader.present()
+
+        if(this.guestCheckoutForm.valid){
+          // Save customer as normal
+          let customer: Customer = {
+            FirstName : this.guestCheckoutForm.controls['guest-first-name'].value,
+            LastName : this.guestCheckoutForm.controls['guest-last-name'].value,
+            EMail : this.guestCheckoutForm.controls['guest-email'].value,
+            IsGuest : true
+          }
+
+          // Redirect to checkout review
+          this.storage.set('user', customer).then(() => {
+            this.orderService.customerInfo = customer;
+                this.hideGuestForm = true
+                this.userLoggedIn = true;
+                loader.dismiss();
+          });
+        }
       }
-  }
-  ionViewDidLoad() {
-
-    console.log('ionViewDidLoad CheckoutReviewPage');
-  }
-
-  // must be present with auto-unsubscribe even if empty
-  ngOnDestroy() {
-    // You can also do whatever you need here
-  }
-  removeItem(item, index) {
-    console.log("Item Removed");
-    let alert = this.alertCtrl.create({
-        title: "Confirm",
-        message: "Are you sure you want to remove this item from your bag?",
-        buttons: [
-            {
-                text: "No.",
-                role: "cancel",
-                cssClass: "button-cancel",
-                handler: () => {
-                    console.log("Cancel clicked");
-                }
-            },
-            {
-                text: "Yes.",
-                cssClass: "button-accept",
-                handler: () => {
-                    console.log("Removed clicked");
-                    this.bag.removeFromBagAtIndex(index);
-                }
-            }
-        ]
-    });
-    alert.present();
-  }
-  public putOrder(){
-    // Ensure time was selected
-    if(!this.selectedTime){
-      this.submitAttempted = true;
-      return;
+    getCustomer() {
+        // get customer so order PUT doesn't fail
+        this.storage.get('user').then(customerData => {
+            this.orderService.customerInfo = customerData;
+            this.userLoggedIn = true;
+        })
     }
 
-    // this.processing = true;
+    getAddress() {
 
-
-    // this.orderService.putOrder(this.bagItems).subscribe(response => {
-
-    //   if(response.ResultCode === 0 || response.ResultCode === 4){
-    //     // Save to LocalStorage and route to checkout
-    //     this.orderService.saveOrderToLocalStorage(response).subscribe(result => {
-    //       if(result){
-    //         this.router.navigate(['/checkout/payment']);
-    //       }
-    //     });
-    //   }
-    //   else{
-    //     this.processing = false;
-    //     this.errorData.error = "We're sorry. There was an error placing your order. Please try again."
-    //     this.wpService.logError('Put Order Error: ' + JSON.stringify(response)).subscribe(() => {});
-    //   }
-    // }, error => {
-    //   this.processing = false;
-    //   this.errorData.error = "We're sorry. There was an error placing your order. Please try again."
-    //   this.wpService.logError('Put Order Error: ' + JSON.stringify(error)).subscribe(() => {});
-    // });
-  }
-  private retrievePickupTimes(){
-    this.orderService.retrieveTimes('1').subscribe(times => {
-      // 0 position in array indicates current day
-      console.log('times', times)
-      this.times = JSON.parse(times._body)[0].Value;
-      console.log('this.time', this.times)
-    })
-  }
-
-  private getNextAvailableTime(){
-    this.orderService.getNextAvailableTime().subscribe(nextTime => {
-      console.log('next time', nextTime)
-        this.selectedTime = nextTime;
-    })
-  }
-
-  public timeSelectChanged(){
-      console.log(this.timeSelectBox)
-    switch(this.timeSelectBox){
-      case 'next':
-        this.getNextAvailableTime();
-      break;
-      default:
-        this.orderService.promiseDateTime = this.timeSelectBox;
-        this.selectedTime = this.timeSelectBox;
-      break;
+        // Get address data from post
+        if (!this.wpService.addressContent) {
+            this.wpService.getAddressContent().subscribe(addressData => {
+                this.addressData = addressData;
+                this.wpService.addressContent = addressData;
+            })
+        } else {
+            this.addressData = this.wpService.addressContent;
+        }
     }
-  }
+    ionViewDidLoad() {
+
+        console.log('ionViewDidLoad CheckoutReviewPage');
+    }
+
+    // must be present with auto-unsubscribe even if empty
+    ngOnDestroy() {
+        // You can also do whatever you need here
+    }
+
+    removeItem(item, index) {
+        console.log("Item Removed");
+        let alert = this.alertCtrl.create({
+            title: "Confirm",
+            message: "Are you sure you want to remove this item from your bag?",
+            buttons: [{
+                    text: "No.",
+                    role: "cancel",
+                    cssClass: "button-cancel",
+                    handler: () => {
+                        console.log("Cancel clicked");
+                    }
+                },
+                {
+                    text: "Yes.",
+                    cssClass: "button-accept",
+                    handler: () => {
+                        console.log("Removed clicked");
+                        this.bag.removeFromBagAtIndex(index);
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    public putOrder() {
+        // Ensure time was selected
+        if (!this.selectedTime) {
+            this.submitAttempted = true;
+            return;
+        }
+
+        this.processing = true;
+
+
+        this.orderService.putOrder(this.itemsInBag).subscribe(response => {
+            console.log('here response', response.json())
+            let jsonResponse = response.json()
+              if(jsonResponse.ResultCode === 0 || jsonResponse.ResultCode === 4){
+                // Save to LocalStorage and route to checkout
+                this.orderService.saveOrderToLocalStorage(jsonResponse).then(result => {
+                  if(result){
+                    // this.router.navigate(['/checkout/payment']);
+                    console.log('go to payment page')
+                  }
+                });
+                console.log('TRUEEE')
+              }
+              else{
+                this.processing = false;
+                this.errorData.error = "We're sorry. There was an error placing your order. Please try again."
+                this.wpService.logError('Put Order Error: ' + JSON.stringify(response)).subscribe(() => {});
+              }
+        }, error => {
+           this.processing = false;
+           this.errorData.error = "We're sorry. There was an error placing your order. Please try again."
+           this.wpService.logError('Put Order Error: ' + JSON.stringify(error)).subscribe(() => {});
+        });
+    }
+
+
+    private retrievePickupTimes() {
+        this.orderService.retrieveTimes('1').subscribe(times => {
+
+            // 0 position in array indicates current day
+            this.times = JSON.parse(times._body)[0].Value;
+        })
+    }
+
+    private getNextAvailableTime() {
+        this.orderService.getNextAvailableTime().subscribe(nextTime => {
+
+            this.selectedTime = nextTime;
+
+        })
+    }
+
+    public timeSelectChanged() {
+        // console.log(this.timeSelectBox)
+        switch (this.timeSelectBox) {
+            case 'next':
+                this.getNextAvailableTime();
+                break;
+            default:
+                this.orderService.promiseDateTime = this.timeSelectBox;
+                this.selectedTime = this.timeSelectBox;
+                break;
+        }
+    }
 
 }
