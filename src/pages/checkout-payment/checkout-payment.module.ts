170c2fd9f0c249533356d4703c13ffaafde67a57
Jacob Greenwald <jeg4t@virginia.edu>
2018-10-17 16:56:09 -0400

    add directives module

--- src/pages/checkout-payment/checkout-payment.module.ts
+++ src/pages/checkout-payment/checkout-payment.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CheckoutPaymentPage } from './checkout-payment';
+import { DirectivesModule } from '../../directives/directives.module';
 
 @NgModule({
   declarations: [
     CheckoutPaymentPage,
   ],
   imports: [
+    DirectivesModule,
     IonicPageModule.forChild(CheckoutPaymentPage),
   ],
   exports: [
