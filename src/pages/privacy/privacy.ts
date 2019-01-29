9c5bdf93cc37223ac986a4bdf51e2e5593c36bba
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-29 10:58:04 -0500

    adding privacy policy page, pulling post from wordpress api and styling

---
+++ src/pages/privacy/privacy.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, LoadingController } from 'ionic-angular';
+import { WordPressProvider } from '../../providers/word-press/word-press';
+import { IPost } from '../../models/post';
+import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
+
+@AutoUnsubscribe()
+
+@IonicPage()
+
+
+@Component({
+  selector: 'page-privacy',
+  templateUrl: 'privacy.html',
+  providers: [WordPressProvider]
+})
+export class PrivacyPage {
+
+    public page: any;
+    public pageContent : IPost;
+
+    constructor(
+
+        private wordpressService: WordPressProvider,
+        private loadingController: LoadingController,
+
+    ) { }
+
+    ngOnInit() {
+        this.getPrivacyPolicy();
+    }
+
+    // must be present with auto-unsubscribe even if empty
+    ngOnDestroy() {
+        // You can also do whatever you need here
+    }
+
+    public getPrivacyPolicy () {
+        let loader = this.loadingController.create({ content: "Loading" });
+        loader.present()
+
+        this.wordpressService.getPost(3982).subscribe(page => {
+            this.pageContent = page;
+            loader.dismiss();
+        })
+    }
+
+}
