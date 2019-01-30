5f240cad9372684cf6ed36550cad9b81ca05d073
Jacob Greenwald <jeg4t@virginia.edu>
2019-01-30 14:53:07 -0500

    importing recapcha module and setting site key

--- src/pages/contact-us/contact-us.module.ts
+++ src/pages/contact-us/contact-us.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ContactUsPage } from './contact-us';
 import { PipesModule } from '../../pipes/pipes.module';
+import { RECAPTCHA_SETTINGS, RecaptchaSettings, RecaptchaModule } from 'ng-recaptcha';
+import { RecaptchaFormsModule } from 'ng-recaptcha/forms';
 
 @NgModule({
   declarations: [
@@ -9,10 +11,16 @@ import { PipesModule } from '../../pipes/pipes.module';
   ],
   imports: [
     IonicPageModule.forChild(ContactUsPage),
-    PipesModule
+    PipesModule,
+    RecaptchaModule,
+    RecaptchaFormsModule
   ],
   exports: [
     ContactUsPage
-  ]
+  ],
+  providers: [{
+    provide: RECAPTCHA_SETTINGS,
+    useValue: { siteKey: '6LefV3EUAAAAAH7Xzi3J1jRu4y-W3usgOdOu9xlr' } as RecaptchaSettings,
+  }],
 })
 export class ContactUsPageModule {}
