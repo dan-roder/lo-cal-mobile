84666d9845fc51b2e012f1683c52c196523c2c63
Dan Roder <dsroder@gmail.com>
2019-02-04 18:20:24 -0500

    added all password reset related functionality

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -3,10 +3,11 @@ import { NavController, NavParams, IonicPage, LoadingController, Loading } from 
 import { AuthProvider } from '../../providers/auth/auth';
 import { FormBuilder, Validators, FormGroup, AbstractControl } from '@angular/forms';
 import { Storage } from "@ionic/storage";
-import { Customer, RailsUpdate } from '../../models/customer';
+import { Customer, RailsUpdate, InLoginUpdate } from '../../models/customer';
 import { CustomerProvider } from '../../providers/customer/customer';
 import { SavedPayment } from '../../models/Payment';
 import { WordPressProvider } from '../../providers/word-press/word-press';
+import { PasswordMatch } from '../../utils/passwordmatch';
 
 
 @IonicPage()
@@ -17,13 +18,17 @@ import { WordPressProvider } from '../../providers/word-press/word-press';
 export class AccountPage {
   public accountForm: FormGroup;
   public questionForm: FormGroup;
+  public passwordForm: FormGroup;
   public errorOccurred: boolean = false;
   public accountError: string = '';
+  public passwordError: string = '';
+  public passwordSuccess: string = '';
   public customer : Customer;
   public customerId: string = '';
   public securityQuestion : string = '';
   public savedPayments : SavedPayment;
   public editing: boolean = false;
+  public editingPassword: boolean = false;
   public accountProcessing: boolean = false;
 
   constructor(
@@ -36,6 +41,7 @@ export class AccountPage {
     private wpService: WordPressProvider,
     private loading: LoadingController
   ) {
+    // Set up Account Form
     this.accountForm = this.fb.group({
       'first-name' : [null, Validators.required],
       'last-name' : [null, Validators.required],
@@ -49,14 +55,28 @@ export class AccountPage {
         'zip' : [null, [Validators.required, Validators.pattern('^[0-9]{5}$')]]
       })
     });
+
+    // Set up Password Reset Form
+    this.passwordForm = fb.group({
+      'old-password' : ['', Validators.required],
+      'password' : ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)])],
+      'confirm-password' : ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+    }, {
+      validator : PasswordMatch.MatchPassword
+    });
   }
 
   ionViewCanEnter(){
-    this.authService.authenticated().then(response => {
-      if(response === null || response === undefined || response.CustomerId === undefined){
-        this.navCtrl.push('LoginPage');
-      }
-    });
+    console.log(this.authService.authenticated());
+    // this.authService.authenticated().then(response => {
+    //   if(response === null || response === undefined || response.CustomerId === undefined){
+    //     this.navCtrl.push('LoginPage');
+    //     return false;
+    //   }
+    //   else{
+    //     return true;
+    //   }
+    // });
   }
 
   ionViewDidLoad() {
@@ -74,6 +94,8 @@ export class AccountPage {
       });
     });
   }
+
+
 
   private patchAccountForm(customerData){
     this.accountForm.patchValue({
@@ -168,6 +190,53 @@ export class AccountPage {
         this.patchAccountForm(this.customer);
       })
     }
+  }
+
+  public editPassword(){
+    this.editingPassword = !this.editingPassword;
+    this.passwordError = '';
+    this.passwordSuccess = '';
+
+    if(!this.editingPassword){
+      this.passwordForm.patchValue({
+        'old-password' : '',
+        'password' : '',
+        'confirm-password' : ''
+      });
+    }
+  }
+
+  public updatePassword(formData){
+    if(formData.valid){
+      let loginInfo : InLoginUpdate = {
+        Email : this.customer.EMail,
+        OldPassword : formData.get('old-password').value,
+        NewPassword : formData.get('password').value
+      }
+
+      this.customerService.updateLoginInfo(loginInfo).subscribe((result) => {
+        this.passwordError = '';
+
+        if(result.ok){
+          this.editingPassword = false;
+          this.passwordSuccess = 'Password successfully changed. You will now be logged out and need to log in again.';
+          this.destroyAndLogout();
+        }
+      }, (error) => {
+        const errorObj = error.json();
+        this.passwordError = errorObj.message;
+      });
+    }
+  }
+
+  private destroyAndLogout(){
+    setTimeout(() => {
+      this.localStorage.remove('user').then(() => {
+        console.log('user object removed');
+        // TODO: SEND USER BACK TO INITIAL PAGE
+        // this.router.navigateByUrl('/login');
+      });
+    }, 4000);
   }
 
 }
