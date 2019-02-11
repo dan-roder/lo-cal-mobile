e33486c5f6923414613ea7215fb4855a767c6896
Dan Roder <dsroder@gmail.com>
2019-02-11 17:59:16 -0500

    added card type pipe to pipes module

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -7,25 +7,28 @@ import { AllergensPipe } from './allergens.pipe';
 import { CardImagePipe } from './image.pipe';
 import { SafeHtml } from './safe-html.pipe';
 import { FeaturedImagePipe } from './featured-image.pipe';
+import { CardTypePipe } from './card-type.pipe';
 @NgModule({
-    declarations: [
-        TruncatePipe,
-        TrimHtmlPipe,
-        NoXPipe,
-        AllergensPipe,
-        CardImagePipe,
-        SafeHtml,
-        FeaturedImagePipe,
-    ],
-    imports: [CommonModule],
-    exports: [
-        TruncatePipe,
-        TrimHtmlPipe,
-        NoXPipe,
-        AllergensPipe,
-        CardImagePipe,
-        SafeHtml,
-        FeaturedImagePipe,
-    ]
+  declarations: [
+    TruncatePipe,
+    TrimHtmlPipe,
+    NoXPipe,
+    AllergensPipe,
+    CardImagePipe,
+    SafeHtml,
+    FeaturedImagePipe,
+    CardTypePipe
+  ],
+  imports: [CommonModule],
+  exports: [
+    TruncatePipe,
+    TrimHtmlPipe,
+    NoXPipe,
+    AllergensPipe,
+    CardImagePipe,
+    SafeHtml,
+    FeaturedImagePipe,
+    CardTypePipe
+  ]
 })
 export class PipesModule { }
