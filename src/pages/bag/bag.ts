2d1809e7bce0d83978f7acca29851ff8bfc66f04
Dan Roder <dsroder@gmail.com>
2019-02-11 18:17:30 -0500

    adjusted indentation and removed unused imports from bag

--- src/pages/bag/bag.ts
+++ src/pages/bag/bag.ts
@@ -1,94 +1,84 @@
 import { Component } from "@angular/core";
-import {
-    Platform,
-    AlertController,
-    IonicPage,
-    NavController,
-    NavParams
-} from "ionic-angular";
-
+import { Platform, AlertController, IonicPage, NavController, NavParams } from "ionic-angular";
 import { BagProvider } from "../../providers/bag/bag";
 import { LineItem } from "../../models/LineItem";
-import { MenuItemPage } from "../../pages/menu-item/menu-item";
-import { CustomerProvider } from "../../providers/customer/customer";
 import { Storage } from "@ionic/storage";
 
 @IonicPage()
 @Component({
-    selector: "page-bag",
-    templateUrl: "bag.html"
+  selector: "page-bag",
+  templateUrl: "bag.html"
 })
 export class BagPage {
 
-    public itemsInBag: Array<LineItem>;
-    public subtotal: number;
-    public tax: number;
-    public total: number;
-    public isLoggedIn: string;
+  public itemsInBag: Array<LineItem>;
+  public subtotal: number;
+  public tax: number;
+  public total: number;
+  public isLoggedIn: string;
 
-    constructor(
-        public platform: Platform,
-        public navCtrl: NavController,
-        public navParams: NavParams,
-        private alertCtrl: AlertController,
-        private bag: BagProvider,
-        private customer: CustomerProvider,
-        private storage: Storage
-    ) {
-        this.platform.ready().then(() => {
-            this.itemsInBag = this.bag.itemsInBag;
-            this.subtotal = this.calculateSubtotal(this.itemsInBag);
-            this.tax = 0.0;
-            this.total = this.subtotal + this.tax;
-        });
-    }
-    ngOnInit() {
-        // check if customer is logged in by pulling customer id from ionic storage
-        this.storage.get("customerid").then(customer => {
-            this.isLoggedIn = customer;
-        });
-    }
+  constructor(
+    public platform: Platform,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private alertCtrl: AlertController,
+    private bag: BagProvider,
+    private storage: Storage
+  ) {
+    this.platform.ready().then(() => {
+      this.itemsInBag = this.bag.itemsInBag;
+      this.subtotal = this.calculateSubtotal(this.itemsInBag);
+      this.tax = 0.0;
+      this.total = this.subtotal + this.tax;
+    });
+  }
 
-    backToMenu() {
-        this.navCtrl.popToRoot();
-    }
+  ngOnInit() {
+    // check if customer is logged in by pulling customer id from ionic storage
+    this.storage.get("customerid").then(customer => {
+      this.isLoggedIn = customer;
+    });
+  }
 
-    calculateSubtotal(bagItems): number {
-        let total = 0.0;
-        bagItems.forEach(item => {
-            total += parseFloat(item.ExtendedPrice);
-        });
-        return total;
-    }
+  backToMenu() {
+    this.navCtrl.popToRoot();
+  }
+
+  calculateSubtotal(bagItems): number {
+    let total = 0.0;
+    bagItems.forEach(item => {
+      total += parseFloat(item.ExtendedPrice);
+    });
+    return total;
+  }
 
     removeItem(item, index) {
-        let alert = this.alertCtrl.create({
-            title: "Confirm",
-            message: "Are you sure you want to remove this item from your bag?",
-            buttons: [
-                {
-                    text: "No.",
-                    role: "cancel",
-                    cssClass: "button-cancel",
-                    handler: () => {
-                        console.log("Cancel clicked");
-                    }
-                },
-                {
-                    text: "Yes.",
-                    cssClass: "button-accept",
-                    handler: () => {
-                        // console.log("Removed clicked");
-                        this.bag.removeFromBagAtIndex(index);
-                        this.subtotal = this.calculateSubtotal(this.itemsInBag);
-                        this.tax = 0.0;
-                        this.total = this.subtotal + this.tax;
-                    }
-                }
-            ]
-        });
-        alert.present();
+      let alert = this.alertCtrl.create({
+        title: "Confirm",
+        message: "Are you sure you want to remove this item from your bag?",
+        buttons: [
+          {
+            text: "No.",
+            role: "cancel",
+            cssClass: "button-cancel",
+            handler: () => {}
+          },
+          {
+            text: "Yes.",
+            cssClass: "button-accept",
+            handler: () => {
+              // console.log("Removed clicked");
+              this.bag.removeFromBagAtIndex(index);
+              this.subtotal = this.calculateSubtotal(this.itemsInBag);
+              this.tax = 0.0;
+              this.total = this.subtotal + this.tax;
+            }
+          }
+        ]
+      });
+      alert.present();
     }
+
     // goToItem(item, index) {
     //     console.log("edit item", item);
 
@@ -97,11 +87,10 @@ export class BagPage {
     // }
 
     checkout(isGuest) {
-
-        if(isGuest) {
-          this.navCtrl.push("CheckoutReviewPage", {guest: true});
-        } else {
-          this.navCtrl.push("CheckoutReviewPage");
-        }
+      if(isGuest) {
+        this.navCtrl.push("CheckoutReviewPage", {guest: true});
+      } else {
+        this.navCtrl.push("CheckoutReviewPage");
+      }
     }
 }
