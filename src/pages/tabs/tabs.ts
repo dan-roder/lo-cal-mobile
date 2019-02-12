204b8e40e3a184873b5e52e2b10c7d6570c5cb89
Dan Roder <dsroder@gmail.com>
2019-02-12 17:55:35 -0500

    removed need for customer service from tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,14 +1,11 @@
 import { Component } from "@angular/core";
 import { App, Platform, IonicPage, NavController, NavParams } from "ionic-angular";
 import { Storage } from "@ionic/storage";
-// import { SuperTabsController } from "ionic2-super-tabs";
 import { MenuProvider } from "../../providers/menu/menu";
 import { BagProvider } from '../../providers/bag/bag';
-import { CustomerProvider } from '../../providers/customer/customer';
 import { LineItem } from '../../models/LineItem';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from "rxjs/Subscription";
-// import { InRegistration } from "../../models/customer";
 import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 
 @AutoUnsubscribe()
@@ -25,8 +22,6 @@ export class TabsComponent {
   public itemsInBag       : Array<LineItem> = [];
   public menuObserver     : Subscription;
   public bagObserver      : Subscription;
-  public customerObserver : Subscription;
-  public currentCustomer;
   public tab0Root         : String = "MenuComponent";
   public tab4Root         : String = "BurgersPage";
 
@@ -37,22 +32,11 @@ export class TabsComponent {
     public  storage   : Storage,
     private menu      : MenuProvider,
     private bag       : BagProvider,
-    private customer  : CustomerProvider,
     private app       : App
   ) {
     this.platform.ready().then( () => {
       this.menuObserver = this.menu.getSubmenus().subscribe( data => {
         this.subMenus = data;
-      });
-
-      this.customerObserver = this.customer.customerId.subscribe( ( customer ) => {
-        console.log( customer );
-        if ( customer ) {
-          this.currentCustomer = customer;
-        } else {
-          console.log( "NO CUSTOMER" );
-          this.currentCustomer = null;
-        }
       });
     });
   }
@@ -81,16 +65,6 @@ export class TabsComponent {
       }
     })
     .catch( error => console.log( error ) );
-
-    this.customerObserver = this.customer.customerId.subscribe( ( customer ) => {
-        console.log( customer );
-        if ( customer ) {
-          this.currentCustomer = customer;
-        } else {
-          console.log( "NO CUSTOMER" );
-          this.currentCustomer = null;
-        }
-    });
   }
 
   // must be present with auto-unsubscribe even if empty
