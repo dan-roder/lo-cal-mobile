fc3f62e0703fba151b7687982208909f651e8a60
Dan Roder <dsroder@gmail.com>
2019-02-12 12:42:47 -0500

    changed indentation

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -16,129 +16,100 @@ import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 @IonicPage()
 
 @Component({
-    selector: "page-tabs",
-    templateUrl: "tabs.html"
+  selector: "page-tabs",
+  templateUrl: "tabs.html"
 })
 export class TabsComponent {
 
-    public subMenus         : Observable<any>;
-    public itemsInBag       : Array<LineItem> = [];
-    public menuObserver     : Subscription;
-    public bagObserver      : Subscription;
-    public customerObserver : Subscription;
-    public currentCustomer;
-    public tab0Root         : String = "MenuComponent";
-    public tab4Root         : String = "BurgersPage";
+  public subMenus         : Observable<any>;
+  public itemsInBag       : Array<LineItem> = [];
+  public menuObserver     : Subscription;
+  public bagObserver      : Subscription;
+  public customerObserver : Subscription;
+  public currentCustomer;
+  public tab0Root         : String = "MenuComponent";
+  public tab4Root         : String = "BurgersPage";
 
-    constructor(
-        public  platform  : Platform,
-        public  navCtrl   : NavController,
-        public  navParams : NavParams,
-        public  storage   : Storage,
-        private menu      : MenuProvider,
-        private bag       : BagProvider,
-        private customer  : CustomerProvider,
-        private app       : App
-    ) {
+  constructor(
+    public  platform  : Platform,
+    public  navCtrl   : NavController,
+    public  navParams : NavParams,
+    public  storage   : Storage,
+    private menu      : MenuProvider,
+    private bag       : BagProvider,
+    private customer  : CustomerProvider,
+    private app       : App
+  ) {
+    this.platform.ready().then( () => {
+      this.menuObserver = this.menu.getSubmenus().subscribe( data => {
+        this.subMenus = data;
+      });
 
-        this.platform.ready().then( () => {
+      this.customerObserver = this.customer.customerId.subscribe( ( customer ) => {
+        console.log( customer );
+        if ( customer ) {
+          this.currentCustomer = customer;
+        } else {
+          console.log( "NO CUSTOMER" );
+          this.currentCustomer = null;
+        }
+      });
+    });
+  }
 
-            this.menuObserver = this.menu.getSubmenus().subscribe( data => {
+  ngOnInit() {
+    this.storage.get('bag').then(bagItemsFromLocalStorage => {
+      if (bagItemsFromLocalStorage) {
+        this.itemsInBag = bagItemsFromLocalStorage;
+      }
+    })
+    .catch(error => {
+      console.log( error );
+    });
+  }
 
-                this.subMenus = data;
-            });
+  ionViewWillEnter() {
+    // prevents view my bag from showing when items are removed and user navigates back to menu
+    this.itemsInBag = this.bag.itemsInBag;
+  }
 
+  ionViewDidLoad() {
+    this.storage.get("intro-done").then( done => {
+      if (!done) {
+        this.storage.set("intro-done", true);
+        this.navCtrl.setRoot("IntroComponent");
+      }
+    })
+    .catch( error => console.log( error ) );
 
-            this.customerObserver = this.customer.customerId.subscribe( ( customer ) => {
+    this.customerObserver = this.customer.customerId.subscribe( ( customer ) => {
+        console.log( customer );
+        if ( customer ) {
+          this.currentCustomer = customer;
+        } else {
+          console.log( "NO CUSTOMER" );
+          this.currentCustomer = null;
+        }
+    });
+  }
 
-                console.log( customer );
-                if ( customer ) {
+  // must be present with auto-unsubscribe even if empty
+  ngOnDestroy() {
+    // You can also do whatever you need here
+  }
 
-                    this.currentCustomer = customer;
+  public onTabSelect(tab: { index: number; id: string }) {
+    let index = tab.index-1;
+  }
 
-                } else {
+  public openBag(page) {
+    this.app.getRootNavs()[0].push(page);
+  }
 
-                    console.log( "NO CUSTOMER" );
-                    this.currentCustomer = null;
-
-                }
-
-            });
-
-        });
-    }
-
-    ngOnInit() {
-        this.storage.get('bag').then(bagItemsFromLocalStorage => {
-
-            if (bagItemsFromLocalStorage) {
-                this.itemsInBag = bagItemsFromLocalStorage;
-            }
-
-        })
-        .catch(error => {
-
-            console.log( error );
-        });
-
-    }
-
-    ionViewWillEnter() {
-        // prevents view my bag from showing when items are removed and user navigates back to menu
-        this.itemsInBag = this.bag.itemsInBag;
-    }
-
-    ionViewDidLoad() {
-
-        this.storage.get("intro-done").then( done => {
-            if (!done) {
-
-                this.storage.set("intro-done", true);
-                this.navCtrl.setRoot("IntroComponent");
-
-            }
-        })
-        .catch( error => console.log( error ) );
-
-        this.customerObserver = this.customer.customerId.subscribe( ( customer ) => {
-
-            console.log( customer );
-            if ( customer ) {
-
-                this.currentCustomer = customer;
-
-            } else {
-
-                console.log( "NO CUSTOMER" );
-                this.currentCustomer = null;
-
-            }
-
-        });
-    }
-
-    // must be present with auto-unsubscribe even if empty
-    ngOnDestroy() {
-        // You can also do whatever you need here
-    }
-
-    public onTabSelect(tab: { index: number; id: string }) {
-
-        let index = tab.index-1;
-
-    }
-
-    public openBag(page) {
-
-        this.app.getRootNavs()[0].push(page);
-
-    }
-
-    public setParams(category) {
-
-        let params = {
-            menuCategory: category
-        };
-        return params;
-    }
+  public setParams(category) {
+    let params = {
+        menuCategory: category
+    };
+    return params;
+  }
 }
