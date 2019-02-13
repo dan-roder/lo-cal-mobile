ac2e3be5f65ba863d4b84bf8b66e61845f3b21e9
Dan Roder <dsroder@gmail.com>
2019-02-13 12:25:54 -0500

    created and styled all order history

--- src/pages/order-history/order-history.ts
+++ src/pages/order-history/order-history.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+import { AuthProvider } from '../../providers/auth/auth';
+import { Customer } from '../../models/customer';
+import { CustomerProvider } from '../../providers/customer/customer';
+import { Order } from '../../models/Order';
 
 /**
  * Generated class for the OrderHistoryPage page.
@@ -8,17 +13,33 @@ import { NavController, NavParams } from 'ionic-angular';
  * on Ionic pages and navigation.
  */
 
+@IonicPage()
+
 @Component({
   selector: 'page-order-history',
   templateUrl: 'order-history.html',
 })
 export class OrderHistoryPage {
+  private currentUser: Customer;
+  public orderHistory: Array<Order>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private authService: AuthProvider, private customerService: CustomerProvider) {
+  }
+
+  ionViewCanEnter(){
+    return this.authService.authenticated().then(response => {
+      if(!response){
+        return false;
+      }
+      this.currentUser = response;
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OrderHistoryPage');
+    this.customerService.getOrderHistory(this.currentUser.CustomerId).subscribe(orderHistory => {
+      const orderHistoryJson = orderHistory.json();
+      this.orderHistory = orderHistoryJson;
+    })
   }
 
 }
