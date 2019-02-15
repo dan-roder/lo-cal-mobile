21cd6d5e9d1bc32fd6ca8094594ae27911bbb6f6
Dan Roder <dsroder@gmail.com>
2019-02-15 11:50:01 -0500

    updated indentation on phone mask

--- src/directives/phone-mask/phone-mask.ts
+++ src/directives/phone-mask/phone-mask.ts
@@ -2,31 +2,26 @@ import { Directive, Output, EventEmitter } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
 @Directive({
-    selector: '[formControlName][phone]', // Attribute selector
-    host: {
-        '(ngModelChange)': 'onInputChange($event)'
-    }
+  selector: '[formControlName][phone]', // Attribute selector
+  host: {
+    '(ngModelChange)': 'onInputChange($event)'
+  }
 })
 export class PhoneMaskDirective {
 
-    constructor(
-        public model: NgControl
-    ) {
+  constructor(
+    public model: NgControl
+  ) {
 
-        console.log('Hello PhoneMaskDirective Directive');
+  }
 
-    }
+  @Output() rawChange: EventEmitter<string> = new EventEmitter<string>();
 
-    @Output() rawChange: EventEmitter<string> = new EventEmitter<string>();
-
-    onInputChange( e, backspace ) {
-
-        let x = e.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-        let rawValue = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-        // new value
-        this.model.valueAccessor.writeValue( rawValue );
-        this.rawChange.emit( rawValue );
-
-    }
-
+  onInputChange( e, backspace ) {
+    let x = e.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+    let rawValue = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+    // new value
+    this.model.valueAccessor.writeValue( rawValue );
+    this.rawChange.emit( rawValue );
+  }
 }
