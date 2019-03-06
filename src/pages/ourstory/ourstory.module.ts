51a978621848ff1b5e65156af45f94ddd3246f32
Jacob Greenwald <jeg4t@virginia.edu>
2019-03-06 16:18:12 -0500

    adding pipes module

--- src/pages/ourstory/ourstory.module.ts
+++ src/pages/ourstory/ourstory.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-
+import { PipesModule } from '../../pipes/pipes.module';
 import { OurstoryComponent } from './ourstory';
 // import { CommonModule } from '@angular/common';
 // import { SharedModule } from '../../app/shared/shared.module';
@@ -11,12 +11,13 @@ import { ComponentsModule } from "../../components/components.module";
     OurstoryComponent
   ],
   imports: [
-    
+
     IonicPageModule.forChild(OurstoryComponent),
     // CommonModule,
     // SharedModule,
+    PipesModule,
     ComponentsModule
-    
+
   ]
 })
 export class OurstoryModule {}
