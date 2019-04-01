5b29796799011684a79fd213d7e5ba9b97957eba
Jacob Greenwald <jeg4t@virginia.edu>
2019-04-01 10:13:24 -0400

    remove unused import

--- src/pages/catering/catering.module.ts
+++ src/pages/catering/catering.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CateringPage } from './catering';
-import { ComponentsModule } from "../../components/components.module";
 import { PipesModule } from '../../pipes/pipes.module';
 
 @NgModule({
