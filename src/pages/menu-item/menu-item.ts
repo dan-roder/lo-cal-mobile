b8590ad134669841b054ab0dc6f011567be48cbb
Dan Roder <dsroder@gmail.com>
2019-04-22 12:27:27 -0400

    clearing customizationData on size change

--- src/pages/menu-item/menu-item.ts
+++ src/pages/menu-item/menu-item.ts
@@ -325,6 +325,7 @@ export class MenuItemPage {
         defaults = this.salesItemDetails.DefaultOptions;
       }
 
+      this.customizationData = {};
       this.orderedSalesItemDetails = this.orderModifierGroups( this.salesItemDetails.ModifierGroups, this.salesItemDetails.ModGroups );
       this.registerCustomizationVariables( this.salesItemDetails.ModGroups, defaults );
     }
