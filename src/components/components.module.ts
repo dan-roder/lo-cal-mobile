39b846ebbd158382ca282599654a234b6fcfa47b
Jacob Greenwald <jeg4t@virginia.edu>
2019-04-26 13:28:11 -0400

    adding new allergens component to components module

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeaturedMediaComponent } from './featured-media/featured-media';
+import { AllergensModalComponent } from './allergens-modal/allergens-modal';
+
 @NgModule({
-	declarations: [FeaturedMediaComponent],
+	declarations: [
+    FeaturedMediaComponent,
+    AllergensModalComponent
+  ],
 	imports: [
 		CommonModule
 	],
-	exports: [FeaturedMediaComponent]
+	exports: [
+    FeaturedMediaComponent,
+    AllergensModalComponent
+  ]
 })
 export class ComponentsModule {}
