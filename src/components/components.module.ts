bfce2a9daead79bad1dd67d49a4ad534940bf69f
Jacob Greenwald <jeg4t@virginia.edu>
2019-04-28 22:25:15 -0400

    remove uneeded component module

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeaturedMediaComponent } from './featured-media/featured-media';
-import { AllergensModalComponent } from './allergens-modal/allergens-modal';
 import { IonicModule } from 'ionic-angular';
 
 @NgModule({
 	declarations: [
-    FeaturedMediaComponent,
-    AllergensModalComponent
+    FeaturedMediaComponent
   ],
 	imports: [
     IonicModule,
 		CommonModule
 	],
 	exports: [
-    FeaturedMediaComponent,
-    AllergensModalComponent
+    FeaturedMediaComponent
   ]
 })
 export class ComponentsModule {}
